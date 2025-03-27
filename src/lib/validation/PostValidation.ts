35ebfcad09087a7eb0bdf1047d746dbbcfc8aee4
CryptoMorki3s <cryptomorki3s@gmail.com>
2025-03-27 10:12:53 +0100

    Initial commit

--- src/lib/validation/PostValidation.ts
+++ src/lib/validation/PostValidation.ts
@@ -4,7 +4,7 @@ export const PostValidation = z.object({
     caption:z.string().min(5, {message: "You've gotta have more than that to say, at least make it 5 characters" }).max(2200, {message: "Your message is too long, please keep it at 2200 characters. ;)"}),
     file:z.custom<File[]>(),
     location: z.string().min(1, {message: "OOPS! should be more than 1 character"}).max(1000, {message: "Nahhh, 1000 characters is the maximum."}),
-    tage: z.string()
+    tags: z.string()
 })
 
 export const CommentValidation = z.object({
